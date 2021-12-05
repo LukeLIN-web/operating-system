f8063c65b92947bec7269471f13d920d0e887525
LukeLIN-web <1263810658@qq.com>
2021-12-06 00:05:41 +0800

    I dont know why , it stuck in VPN2

--- lab/lab4/arch/riscv/kernel/vm.c
+++ lab/lab4/arch/riscv/kernel/vm.c
@@ -7,18 +7,18 @@ extern unsigned long long data_start;
 extern unsigned long long _end;
 
 
-int page_count = 0;
+int page_count = 1;
 int page_size = 4096;
+uint64_t page_offset = 12;
+uint64_t valid = 1;
 void create_mapping(uint64_t *pgtbl, uint64_t va, uint64_t pa, uint64_t sz, int perm){
-    uint64_t page_offset = 12;
-    uint64_t valid = 1;
     puts("begin create mapping\n");
+    uint64_t VPN[3];
+    uint64_t Page_table_entry[3];
+    uint64_t PPN;
+    uint64_t *pgtbl_1;
+    uint64_t *pgtbl_0;
     for(uint64_t i = 0;i < sz/page_size; i++){
-        uint64_t VPN[3];
-        uint64_t Page_table_entry[3];
-        uint64_t PPN;
-        uint64_t *pgtbl_1;
-        uint64_t *pgtbl_0;
         PPN = ((pa + i * page_size)&(0x7fff<<12))>>12;
         VPN[2] = ((va + i * page_size) >>30)&0x1ff;
         VPN[1] = ((va + i * page_size)>>21)&0x1ff;
@@ -27,12 +27,11 @@ void create_mapping(uint64_t *pgtbl, uint64_t va, uint64_t pa, uint64_t sz, int 
         // putullHex(VPN[0]);
         // puts("VPN[1]\n");
         // putullHex(VPN[1]);
-        // puts("1");
         // putullHex(VPN[2]);
 
         if(!pgtbl[VPN[2]]){
-            puts("new entry");
-            pgtbl_1 =(uint64_t *)  ( &_end + 0x1000 * page_count);
+            puts("\nnew entry\n");
+            pgtbl_1 =(uint64_t *)( &_end + 0x1000 * page_count);
             page_count ++;
             // Page_table_entry[2] = (0x1ffffff<<39) + ((uint64_t)pgtbl_1<<10) + (perm<<1) + valid;
             // puti(page_count);
@@ -40,13 +39,17 @@ void create_mapping(uint64_t *pgtbl, uint64_t va, uint64_t pa, uint64_t sz, int 
             pgtbl[VPN[2]] = Page_table_entry[2];
         }
         else{
-            puts("exist entry");
-            pgtbl_1 = (pgtbl[VPN[2]]>>10) << page_offset;
+            puts("\nexist entry   \n");
+            puts("\nputullHex(VPN[2]) =    \n");
+            putullHex(VPN[2]);
+            pgtbl_1 = (uint64_t *)((pgtbl[VPN[2]]>>10) << page_offset);
+            puts("\npgtbl_1 = (pgtbl[VPN[2]]>>10) << page_offset; =    \n");
+            putullHex(pgtbl_1);
         }
-        // puts("2");
+        puts("\n!pgtbl[VPN[2]]  page table finished\n");
 
         if(!pgtbl_1[VPN[1]]){
-            pgtbl_0 = &_end + 0x1000 * page_count;
+            pgtbl_0 = (uint64_t *)(&_end + 0x1000 * page_count);
             // pgtbl_0 = 0x8000000 + 0x1000 * page_count;
             page_count ++; // page count 或许不能全局变量.
             //Page_table_entry[1] = ((uint64_t)0x1ffffff<<39) + (((uint64_t)pgtbl_1>>12)<<10)  + valid;
@@ -54,9 +57,9 @@ void create_mapping(uint64_t *pgtbl, uint64_t va, uint64_t pa, uint64_t sz, int 
             pgtbl_1[VPN[1]] = Page_table_entry[1];
         }
         else{
-            pgtbl_0 = (pgtbl[VPN[1]]>>10) << page_offset;
+            pgtbl_0 =  (uint64_t *)((pgtbl[VPN[1]]>>10) << page_offset);
         }
-        // puts("3");
+        puts("!pgtbl[VPN[1]]  page table finished\n");
         //Page_table_entry[0] = ((uint64_t)0x1ffffff<<39) + ((PPN>>12)<<10) + (perm<<1) + valid;  
         Page_table_entry[0] = ((pa>>12)<<10) + (perm<<1) + valid;
         pgtbl_0[ VPN[0]] = Page_table_entry[0];
